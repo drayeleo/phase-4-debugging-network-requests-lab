c13de8ab2b938c48296e62745e830f97aa2d3c21
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-06-09 10:42:04 -0600

    commit tree ac4415d21ebe8b7bcc1de4c8126dd1cb9adc7066

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,15 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
+    # byebug
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
@@ -23,9 +25,8 @@ class ToysController < ApplicationController
   end
 
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
